fac7eeed4caf0dabfdd36369338bd0971d17c8ff
Talha Akhter <talhaakhter01@gmail.com>
2021-06-05 23:15:56 +0500

    todo app updated

--- public/assignment-todo/app.js
+++ public/assignment-todo/app.js
@@ -2,9 +2,14 @@ var input = document.querySelector("#add");
 var btn = document.querySelector("#btn");
 var list = document.querySelector("#list");
 
-document.addEventListener("keyup", function(event) {
+document.addEventListener("keyup", function (event) {
     if (event.code === 'Enter') {
-        document.getElementById("btn").click();
+        var editInput = document.querySelectorAll('#list input');
+        if (editInput.length) {
+            for (var i = 0; i < editInput.length; i++) {
+                editInput[i].setAttribute('disabled', 'disabled');
+            }
+        }
     }
 });
 
@@ -16,14 +21,27 @@ function addItem() {
         alert('you must write something');
     } else {
         li = document.createElement('li');
-        li.innerHTML = txt;
-        li.setAttribute('onclick', 'onclick(completeItem(this))');
+        li.innerHTML = '<input disabled value=' + txt + ' /><a href="#" onclick=editItem(this)>Edit</a><a href="#" onclick=deleteItem(this)>Delete</a>';
+        // li.setAttribute('onclick', 'onclick(completeItem(this))');
         list.insertBefore(li, list.childNodes[0]);
         input.value = '';
     }
 
 };
 
-function completeItem(item) {
-    item.classList.toggle('checked');
+function editItem(item) {
+    item.previousElementSibling.removeAttribute("disabled");
 };
+
+function deleteItem(item) {
+    console.log(item.parentElement);
+    item.parentElement.remove();
+};
+function deleteAllItem() {
+    var list = document.getElementById('list');
+    while (list.childElementCount != 0) {
+        list.firstChild.remove();
+    }
+};
+
+mdc.ripple.MDCRipple.attachTo(document.querySelector('button'));
