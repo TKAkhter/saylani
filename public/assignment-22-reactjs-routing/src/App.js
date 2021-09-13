809153b4884f378572f0515716e9f5b7eb0b567c
hassan <hassanribery0335@gmail.com>
2021-09-13 23:24:27 +0500

    react router added

--- public/assignment-22-reactjs-routing/src/App.js
+++ public/assignment-22-reactjs-routing/src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import ContactUs from "./components/ContactUs";
+import Blogs from "./components/Blogs";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/About">About</Link>
+                </li>
+                <li>
+                  <Link to="/ContactUs">ContactUs</Link>
+                </li>
+                <li>
+                  <Link to="/Blogs">Blogs</Link>
+                </li>
+              </ul>
+            </nav>
+
+            {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Switch>
+              <Route path="/About">
+                <About />
+              </Route>
+              <Route path="/ContactUs">
+                <ContactUs />
+              </Route>
+              <Route path="/Blogs">
+                <Blogs />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </header>
     </div>
   );
