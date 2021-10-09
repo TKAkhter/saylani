a76fa96e2e1e363f2328fb664d8cca60fa2296fe
Talha Akhter <talhaakhter01@gmail.com>
2021-10-09 20:52:33 +0500

    assignment done

--- public/assignment-25-reactjs-todo/src/components/RealTimeTodo.jsx
+++ public/assignment-25-reactjs-todo/src/components/RealTimeTodo.jsx
@@ -1,43 +1,42 @@
 import { useFormik } from "formik";
 import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
 import * as yup from "yup";
 import { useEffect, useState } from "react";
-import {
-  collection,
-  addDoc,
-  onSnapshot,
-  query,
-  serverTimestamp,
-  orderBy,
-  deleteDoc,
-  doc,
-} from "firebase/firestore";
-import { db } from "../firebase";
+import firebase from "firebase";
+import { getDatabase, ref, push, onValue, remove } from "firebase/database";
 
-const todoCol = collection(db, "todo");
+const db = getDatabase();
 
 const validationSchema = yup.object({
   title: yup.string("Enter your email").required("Email is required"),
 });
 
 async function del(id) {
-  await deleteDoc(doc(todoCol, id));
+  await remove(ref(db, `/realTimeTodo/${id}`));
 }
 
 function RealtimeTodo() {
   const [todo, settodo] = useState([]);
 
   useEffect(() => {
-    const q = query(todoCol, orderBy("timestamp"));
-    const unsubscribe = onSnapshot(q, (snapshot) => {
-      let temp = [];
-      snapshot.forEach((doc) => {
-        let id = doc.id;
-        let data = doc.data();
+    const starCountRef = ref(db, "realTimeTodo/");
+    let temp = [];
+    const unsubscribe = onValue(starCountRef, (snapshot) => {
+      const data = snapshot;
+      // console.log(data);
+      data.forEach((doc) => {
+        let id = doc.key;
+        let data = doc.val();
 
         temp.unshift({
           id: id,
@@ -46,6 +45,7 @@ function RealtimeTodo() {
         });
       });
       settodo(temp);
+      temp = [];
     });
 
     return () => {
@@ -61,12 +61,15 @@ function RealtimeTodo() {
     },
     onSubmit: async (values) => {
       try {
-        const docRef = await addDoc(todoCol, {
-          title: values.title,
-          description: values.description,
-          timestamp: serverTimestamp(),
-        });
-        console.log("Document written with ID: ", docRef.id);
+        function writeUserData(title, description) {
+          push(ref(db, "realTimeTodo/"), {
+            title: values.title,
+            description: values.description,
+            timestamp: firebase.database.ServerValue.TIMESTAMP,
+          });
+        }
+        writeUserData();
+        console.log("Document written with ID: ");
       } catch (e) {
         console.error("Error adding document: ", e);
       }
@@ -81,62 +84,85 @@ function RealtimeTodo() {
       </Typography>
 
       <form onSubmit={formik.handleSubmit}>
-        <Stack spacing={2}>
-          <TextField
-            fullWidth
-            color="primary"
-            id="outlined-basic"
-            label="Title"
-            variant="outlined"
-            name="title"
-            value={formik.values.title}
-            onChange={formik.handleChange}
-            error={formik.touched.title && Boolean(formik.errors.title)}
-            helperText={formik.touched.title && formik.errors.title}
-          />
-
-          <TextField
-            fullWidth
-            color="primary"
-            id="outlined-basic"
-            label="Description"
-            variant="outlined"
-            name="description"
-            value={formik.values.description}
-            onChange={formik.handleChange}
-            error={
-              formik.touched.description && Boolean(formik.errors.description)
-            }
-            helperText={formik.touched.description && formik.errors.description}
-          />
-
-          <Button fullWidth variant="contained" color="primary" type="submit">
-            Add Todo
-          </Button>
-        </Stack>
-      </form>
-
-      <div>
-        {todo.map((eachTodo, i) => {
-          return (
-            <div key={i}>
-              <br />
-              <div> id: {eachTodo.id}</div>
-              <div> title: {eachTodo.title}</div>
-              <div> description: {eachTodo.description}</div>
-
-              <button
-                onClick={() => {
-                  del(eachTodo.id);
-                }}
+        <Paper style={{ margin: 16, padding: 16 }} elevation={3}>
+          <Grid container alignItems="center" spacing={2}>
+            <Grid xs={5} md={10} item style={{ paddingRight: 16 }}>
+              <TextField
+                fullWidth
+                placeholder="Add Todo here"
+                color="primary"
+                id="outlined-basic"
+                label="Task"
+                variant="filled"
+                name="title"
+                value={formik.values.title}
+                onChange={formik.handleChange}
+                error={formik.touched.title && Boolean(formik.errors.title)}
+                helperText={formik.touched.title && formik.errors.title}
+              />
+            </Grid>
+            <Grid xs={5} md={10} item style={{ paddingRight: 16 }}>
+              <TextField
+                fullWidth
+                color="primary"
+                id="outlined-basic"
+                label="Description"
+                variant="filled"
+                name="description"
+                value={formik.values.description}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.description &&
+                  Boolean(formik.errors.description)
+                }
+                helperText={
+                  formik.touched.description && formik.errors.description
+                }
+              />
+            </Grid>
+            <Grid xs={2} md={2} item>
+              <Button
+                fullWidth
+                color="success"
+                variant="contained"
+                type="submit"
               >
-                Delete
-              </button>
-              <br />
-            </div>
-          );
-        })}
-      </div>
+                Add
+              </Button>
+            </Grid>
+          </Grid>
+        </Paper>
+      </form>
+      <Paper style={{ margin: 16, padding: 16 }} elevation={3}>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Recently Added:
+        </Typography>
+        <List spacing={3}>
+          {todo.map((eachTodo) => {
+            return (
+              <Paper style={{ margin: 10 }} elevation={3}>
+                <ListItem
+                  key={eachTodo.id}
+                  secondaryAction={
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon
+                        onClick={() => {
+                          del(eachTodo.id);
+                        }}
+                      />
+                    </IconButton>
+                  }
+                >
+                  <ListItemText
+                    primary={eachTodo.title}
+                    secondary={eachTodo.description}
+                  />
+                </ListItem>
+              </Paper>
+            );
+          })}
+        </List>
+      </Paper>
     </Box>
   );
 }
